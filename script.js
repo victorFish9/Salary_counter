2efd48d6615492757b3ea753d40a5e70f28aab08
Victor <121668012+victorFish9@users.noreply.github.com>
2023-04-03 10:01:49 +0300

    file path changed

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 let sum = 0;
+let evening_increase = 0;
 function Show(){
     var input1 = document.getElementById("startHour").value;
     var input2 = document.getElementById("endHour").value;
-    //document.getElementById("answer").innerHTML = "Answer is " + (input2-input1);
+    
     var a = new Date("12 May, 2018 " + input1);
     var b = new Date("12 May, 2018 " + input2);
     var c = new Date("12 may, 2018 18:00")
     var hours = Math.abs(b - a) / 36e5;
 
     if (b > c){
-        console.log((b - c) / 36e5)
+        evening_increase += Math.abs(b - c) / 36e5;
+        console.log(evening_increase)
+
+        document.getElementById("answer4").innerHTML = "Eveining increase: " + evening_increase;
     }
 
-    document.getElementById("answer").innerHTML = "Workhour is " + hours;
+    document.getElementById("answer").innerHTML = "Workhour: " + Math.round(hours * 100) / 100;
     sum += hours;
-    document.getElementById("answer2").innerHTML = "Sum hour: " +(sum);
+    document.getElementById("answer2").innerHTML = "Sum hour: " + Math.round(sum * 100) / 100;
 }
 function CountSalary(){
     var input1 = document.getElementById("amount").value;
 
     var salary = sum * input1
+    var evening_salary = evening_increase * 1.33;
+    console.log(evening_salary);
 
     document.getElementById("asnwer3").innerHTML = "Salary gonna be " + Math.round(salary);
 }
